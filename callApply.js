4d24b4c18cad54ee38d494df2122a98f9a387f0c
Mowmita Ahmed <ahmedmowmita@gmail.com>
2020-08-10 16:55:31 +0600

    Update

--- callApply.js
+++ callApply.js
@@ -5,8 +5,8 @@ const normalPerson = {
     getFullName: function () {
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function (amount) {
-        this.salary = this.salary - amount;
+    chargeBill: function (amount, tips, tax) {
+        this.salary = this.salary - amount - tips - tax;
         return this.salary;
     }
 }
@@ -35,3 +35,15 @@ console.log(heroPerson.salary);
 const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
 friendlyChargeBill(1500);
 console.log(friendlyPerson.salary);
+
+normalPerson.chargeBill.call(heroPerson, 900, 100, 10);
+normalPerson.chargeBill.call(heroPerson, 3000, 300, 30);
+console.log(heroPerson.salary);
+
+normalPerson.chargeBill.call(friendlyPerson, 5000, 500, 50);
+console.log(friendlyPerson.salary);
+console.log(normalPerson.salary);
+
+normalPerson.chargeBill.apply(heroPerson, [3000, 300, 30]);
+normalPerson.chargeBill.apply(heroPerson, [4000, 400, 40]);
+console.log(heroPerson.salary);
