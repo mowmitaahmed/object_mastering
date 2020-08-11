085aa497b9988de20991ba271c447cb26aece054
Mowmita Ahmed <ahmedmowmita@gmail.com>
2020-08-11 14:30:31 +0600

    Time zone

---
+++ timeOut.js
@@ -0,0 +1,10 @@
+setTimeout(function () {
+    console.log('Lazy Loading');
+}, 5000)
+
+console.log(2222);
+console.log(4444);
+
+setInterval(function () {
+    console.log('doing it again');
+}, 3000)
---
+++ timeZone.js
@@ -0,0 +1,7 @@
+const start = new Date();
+let sum = 0;
+for (let i = 0; i < 100000000; i++) {
+    sum++;
+}
+const end = new Date();
+console.log('Time elapsed', end-start, sum);
